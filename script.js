8346844b3691cdc8b524b12407ead2cb8bec81ea
Victoria Diaz <toridiaz@Victorias-MacBook-Pro.local>
2016-09-23 11:55:27 -0700

    Updated for JSON
    Updated script to accommodate JSON usage

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
 	$('#compoundSelect').load('nameRefreshConnectCompound.php');
 		setInterval(function() {
 			$('#compoundSelect').load('nameRefreshConnectCompound.php')
-	}, 36000);
+	}, 3600000);
 });
 
 // function showHint(str) {
@@ -42,22 +42,64 @@ function nextButton() {
 	searchTable();
 }
 
+ajx = "";
+
 function searchTable() {
 	var start = 0;
-	start = newstart;	
+	start = newstart;
 	tname = $("#table").val();
 	bname = $("#bacteriaid").val();
 	mname = $("#mainsource").val();
 	cname = $("#compound").val();
-	ajx = "loadTable.php?table=" + tname + "&bid=" + bname + "&msource=" + mname + "&cpound=" + cname + "&start=" + start;
-	$.ajax(ajx)
+	dest = "loadTablejson.php?table=";
+	ajx = tname + "&bid=" + bname + "&msource=" + mname + "&cpound=" + cname + "&start=" + start;
+ 	dest = dest + ajx;
+	$.ajax(dest)
         .done(function(results) {
-            $("#answer").html(results);
             results = JSON.parse(results);
-            var html = "";
-            for (i in results) {
-                html += i;
-            }
-            $("#answer").html(html);
+            var htmlcount = "Result set has " + results["count"] + " rows.";
+            $("#answer").html(htmlcount);
+            popTable(results["header"], results["data"]);
         });
 }
+
+function popTable(dataH, dataD) {
+	var html = "";
+	html += "<table>";
+	html += "<tr>";
+	//table heading
+	for (var d in dataH) {
+		item = dataH[d];
+		if (item == "null") {
+			item = "-";
+		}
+        html += "<th>" + item + "</th>";
+   	}
+   	html += "</tr>";
+   	
+   	//table data
+	for (var d in dataD) {
+		html += "<tr>";
+		for (var i in dataD[d]) {
+			var item = dataD[d][i];
+            if (item == "null") {
+                item = "-";
+            }
+            html += "<td>" + item + "</td>";
+        }
+        html += "</tr>";
+    }
+    html += "</table>";
+	$("#answer").html(html);
+}
+
+
+ function download() {
+	var filename = prompt("Save As: ", "filename");
+	var slocation = prompt("Enter file path to save in: \n   Mac-- Users/myname/Downloads/ \n   PC--   C:\\Users\\myname\\Downloads\\ ", "/Users/myname/Downloads/");
+ 	downdest = "export.php?table=";
+ 	downdest = downdest + ajx;
+ 	downdest = downdest + "&outname=" + filename + "&path=" + slocation;
+ 	$.ajax(downdest)
+ 	
+ }
