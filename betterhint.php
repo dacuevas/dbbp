5571d49933c60830ef73853f9795c6f1a7c6f8c5
Victoria Diaz <toridiaz07@gmail.com>
2016-10-22 17:34:57 -0700

    Update for Plates
    Plate_1 and Plate_2 and now Plates, fixed merge for hint search

--- betterhint.php
+++ betterhint.php
@@ -23,19 +23,15 @@ $column = $_REQUEST["r"];
         }
 } else {
 
- if ($column != "BacteriaID") {
- 	$sql = "SELECT DISTINCT $column FROM Plate_1";
+//---------------------------- Plates doesn't have BacteriaIDs
+ if ($column != "BacteriaID") { 
+ 	$sql = "SELECT DISTINCT $column FROM Plates";
  	$result = $conn->query($sql);
  	while ($row = $result->fetch_assoc()) {
          	array_push($array, $row["$column"]);
          }
- 
- 	$sql = "SELECT DISTINCT $column FROM Plate_2";
- 	$result = $conn->query($sql);
- 	while ($row = $result->fetch_assoc()) {
-         	array_push($array, $row["$column"]);
-         }
- }
+   }
+ //-----------------------------
  
  $sql = "SELECT DISTINCT $column FROM GrowthParameters";
  $result = $conn->query($sql);
